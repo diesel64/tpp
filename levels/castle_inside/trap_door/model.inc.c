5c82af9950de2c42575debce17c440a51ff051b1
diesel64 <supershanio64@gmail.com>
2022-09-25 04:43:52 -0400

    Add files via upload
    - Fixed carpet lighting (again)

--- levels/castle_inside/trap_door/model.inc.c
+++ levels/castle_inside/trap_door/model.inc.c
@@ -1,7 +1,7 @@
 // 0x0703BAB0 - 0x0703BAC8
 static const Lights1 inside_castle_seg7_lights_0703BAB0 = gdSPDefLights1(
-    0x3f, 0x3f, 0x3f,
-    0xff, 0xff, 0xff, 0x28, 0x28, 0x28
+    0x37, 0x37, 0x37,
+    0xDD, 0xDD, 0xDD, 0x28, 0x28, 0x28
 );
 
 // 0x0703BAC8 - 0x0703BBB8
@@ -36,11 +36,6 @@ static const Vtx inside_castle_seg7_vertex_0703BBB8[] = {
 
 // 0x0703BC28 - 0x0703BCB8
 static const Gfx inside_castle_seg7_dl_0703BC28[] = {
-    gsDPSetTextureImage(G_IM_FMT_RGBA, G_IM_SIZ_16b, 1, inside_09005000),
-    gsDPLoadSync(),
-    gsDPLoadBlock(G_TX_LOADTILE, 0, 0, 32 * 32 - 1, CALC_DXT(32, G_IM_SIZ_16b_BYTES)),
-    gsSPLight(&inside_castle_seg7_lights_0703BAB0.l, 1),
-    gsSPLight(&inside_castle_seg7_lights_0703BAB0.a, 2),
     gsSPVertex(inside_castle_seg7_vertex_0703BAC8, 15, 0),
     gsSP2Triangles( 0,  1,  2, 0x0,  3,  4,  5, 0x0),
     gsSP2Triangles( 3,  5,  6, 0x0,  7,  8,  9, 0x0),
@@ -54,18 +49,14 @@ static const Gfx inside_castle_seg7_dl_0703BC28[] = {
 
 // 0x0703BCB8 - 0x0703BD28
 const Gfx inside_castle_seg7_dl_0703BCB8[] = {
-    gsDPPipeSync(),
-    gsDPSetCombineMode(G_CC_MODULATERGB, G_CC_MODULATERGB),
-    gsSPClearGeometryMode(G_SHADING_SMOOTH),
-    gsDPSetTile(G_IM_FMT_RGBA, G_IM_SIZ_16b, 0, 0, G_TX_LOADTILE, 0, G_TX_WRAP | G_TX_NOMIRROR, G_TX_NOMASK, G_TX_NOLOD, G_TX_WRAP | G_TX_NOMIRROR, G_TX_NOMASK, G_TX_NOLOD),
-    gsSPTexture(0xFFFF, 0xFFFF, 0, G_TX_RENDERTILE, G_ON),
-    gsDPTileSync(),
-    gsDPSetTile(G_IM_FMT_RGBA, G_IM_SIZ_16b, 8, 0, G_TX_RENDERTILE, 0, G_TX_WRAP | G_TX_NOMIRROR, 5, G_TX_NOLOD, G_TX_WRAP | G_TX_NOMIRROR, 5, G_TX_NOLOD),
-    gsDPSetTileSize(0, 0, 0, (32 - 1) << G_TEXTURE_IMAGE_FRAC, (32 - 1) << G_TEXTURE_IMAGE_FRAC),
+	gsSPClearGeometryMode(G_SHADING_SMOOTH),
+	gsSPDisplayList(mat_castle_inside_CarpetMaterial),
     gsSPDisplayList(inside_castle_seg7_dl_0703BC28),
-    gsSPTexture(0xFFFF, 0xFFFF, 0, G_TX_RENDERTILE, G_OFF),
-    gsDPPipeSync(),
-    gsDPSetCombineMode(G_CC_SHADE, G_CC_SHADE),
-    gsSPSetGeometryMode(G_SHADING_SMOOTH),
-    gsSPEndDisplayList(),
+	gsSPSetGeometryMode(G_SHADING_SMOOTH),
+	gsDPPipeSync(),
+	gsSPSetGeometryMode(G_LIGHTING),
+	gsSPClearGeometryMode(G_TEXTURE_GEN),
+	gsDPSetCombineLERP(0, 0, 0, SHADE, 0, 0, 0, ENVIRONMENT, 0, 0, 0, SHADE, 0, 0, 0, ENVIRONMENT),
+	gsSPTexture(65535, 65535, 0, 0, 0),
+	gsSPEndDisplayList(),
 };
